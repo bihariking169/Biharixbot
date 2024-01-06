6ae00a4b4577bcb4c5980b1ac2db94c8836c9881
ALONE HAPPY <144148196+bihariking169@users.noreply.github.com>
2024-01-06 17:19:42 +0530

    Update welcome.py

--- BrandrdXMusic/plugins/tools/welcome.py
+++ BrandrdXMusic/plugins/tools/welcome.py
@@ -3,7 +3,7 @@ from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
 from pyrogram.types import *
 from logging import getLogger
-from DAXXMUSIC import app
+from BrandrdXMusic import app
 
 
 
@@ -52,13 +52,13 @@ def circle(pfp, size=(500, 500)):
     return pfp
 
 def welcomepic(pic, user, chatname, id, uname):
-    background = Image.open("DAXXMUSIC/assets/wel2.png")
+    background = Image.open("BrandrdXMusic/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp)
     pfp = pfp.resize((825, 824))
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype('DAXXMUSIC/assets/font.ttf', size=110)
-    welcome_font = ImageFont.truetype('DAXXMUSIC/assets/font.ttf', size=60)
+    font = ImageFont.truetype('BrandrdXMusic/assets/font.ttf', size=110)
+    welcome_font = ImageFont.truetype('BrandrdXMusic/assets/font.ttf', size=60)
     draw.text((2100, 1420), f'ID: {id}', fill=(12000, 12000, 12000), font=font)
     pfp_position = (1990, 435)
     background.paste(pfp, pfp_position, pfp)
